89e369437bbe2c0303cf89ca15b1450a8e9df16b
harsita12 <tanuharsita@gmail.com>
2024-08-13 21:58:39 +0530

    final chnages done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harsita12/Web-Apps-Docker-Kubernetes
+
+go 1.21.10
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/home.html")
+}
+
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contact html page
+	http.ServeFile(w, r, "static/courses.html")
+}
+
+func main() {
+
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/courses", coursePage)
+	
+	
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
